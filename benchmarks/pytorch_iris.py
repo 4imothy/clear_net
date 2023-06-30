001da2ae5b89721dc2318ee1fe97058459439bf8
Timothy Cronin <tcroniniv@gmail.com>
2023-06-30 17:37:21 -0400

    add pytorch to bench against

---
+++ benchmarks/pytorch_iris.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# Load the Iris dataset
+iris = load_iris()
+features = iris.data
+labels = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Scale the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Convert data to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).view(-1, 1)
+
+# Define the neural network
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Set hyperparameters
+input_size = X_train.shape[1]
+hidden_size = 16
+output_size = 1
+learning_rate = 0.01
+num_epochs = 10000
+
+# Create the neural network
+net = Net(input_size, hidden_size, output_size)
+
+# Define the loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    # Forward pass
+    outputs = net(X_train)
+    loss = criterion(outputs, y_train)
+
+    # Backward pass and optimization
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (loss.item() < 0.01):
+        break
+
+    # Print the loss for every 10 epochs
+    if (epoch + 1) % 10 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
