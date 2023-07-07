444510b9973da8a250dcdcaf5efcfb39d4b0b6d2
Timothy Cronin <timothy@Timothys-MacBook-Pro.local>
2023-07-07 12:35:55 -0400

    use minimum size

--- examples/linear_regression.c
+++ examples/linear_regression.c
@@ -1,5 +1,6 @@
 #define CLEAR_NET_IMPLEMENTATION
-// #define CLEAR_NET_RATE 0.01f
+// need negative outputs
+#define CLEAR_NET_ACT_OUTPUT Tanh
 #include "../clear_net.h"
 // function to learn
 // y = 2 + 4a - 3b + 5c + 6d - 2e + 7f - 8g + 9h
@@ -14,16 +15,20 @@ float rand_rangef(float lower, float upper) {
 }
 
 #define do_func(a, b, c, d, e, f, g, h)                                        \
-    (2 + 4 * (a)-3 * (b) + 5 * (c) + 6 * (d)-2 * (e) + 7 * (f)-8 * (g) +       \
+  (2 + 4 * (a) - 3 * (b) + 5 * (c) + 6 * (d) -2 * (e) + 7 * (f)-8 * (g) + \
      9 * (h))
 
-const float lower = -100;
-const float upper = 100;
+const float len = 1;
+const float lower = -1.0f * len;
+const float upper = len;
 
+// const float max = do_func(upper, upper, upper, upper, upper, upper, upper, upper);
 const float max =
-    do_func(upper, upper, upper, upper, upper, upper, upper, upper);
+    do_func(1, 1, 1, 1, 1, 1, 1, 1);
 
-float mul_max(float x) { return (x * 2 * max) - max; }
+const size_t dim_input = num_var - 1;
+
+float mul_max(float x) { return (x * max * upper - (2 * upper)); }
 
 int main(void) {
     srand(0);
@@ -37,7 +42,8 @@ int main(void) {
     float h;
     for (size_t i = 0; i < num_train; ++i) {
         for (size_t j = 0; j < num_var; ++j) {
-            train[i * num_var + j] = rand_rangef(lower, upper);
+		  train[i * num_var + j] = rand_rangef(lower, upper);
+		  val[i * num_var + j] = rand_rangef(lower, upper);
             if (j == num_var - 1) {
                 a = train[i * num_var];
                 b = train[i * num_var + 1];
@@ -48,15 +54,9 @@ int main(void) {
                 g = train[i * num_var + 6];
                 h = train[i * num_var + 7];
                 train[i * num_var + num_var - 1] =
-                    (do_func(a, b, c, d, e, f, g, h) + max) / (max * 2);
-            }
-        }
-    }
-    for (size_t i = 0; i < num_train; ++i) {
-        for (size_t j = 0; j < num_var; ++j) {
-            val[i * num_var + j] = rand_rangef(lower, upper);
-            if (j == num_var - 1) {
-                a = val[i * num_var];
+				  do_func(a, b, c, d, e, f, g, h) / max;
+				
+	    	    a = val[i * num_var];
                 b = val[i * num_var + 1];
                 c = val[i * num_var + 2];
                 d = val[i * num_var + 3];
@@ -65,21 +65,21 @@ int main(void) {
                 g = val[i * num_var + 6];
                 h = val[i * num_var + 7];
                 val[i * num_var + num_var - 1] =
-                    (do_func(a, b, c, d, e, f, g, h) + max) / (max * 2);
+				  do_func(a, b, c, d, e, f, g, h) / max;
             }
         }
     }
 
-    size_t dim_input = num_var - 1;
     Matrix input = mat_form(num_train, dim_input, num_var, train);
     Matrix output = mat_form(num_train, 1, num_var, &train[dim_input]);
     Matrix val_in = mat_form(num_train, dim_input, num_var, val);
     Matrix val_out = mat_form(num_train, 1, num_var, &val[dim_input]);
-    size_t  shape[] = {dim_input, 16, 5, 5, 1};
+    // size_t  shape[] = {dim_input, 16, 5, 5, 1};
+    size_t shape[] = {dim_input, 1};
     Net net = alloc_net(shape, ARR_LEN(shape));
     net_rand(net, -1, 1);
     size_t num_epochs = 200000;
-    float error_break = 0.001f;
+    float error_break = 0.01f;
     float error;
     for (size_t i = 0; i < num_epochs; ++i) {
         net_backprop(net, input, output);
@@ -92,14 +92,12 @@ int main(void) {
             break;
         }
     }
-    NET_PRINT(net);
-    /* net_print_results(net, input, output, &mul_max); */
-    /* char *file_name = "lin_reg_model"; */
-    /* net_save_to_file(file_name, net); */
-    /* dealloc_net(&net); */
-    /* net = alloc_net_from_file(file_name); */
-    /* printf("After Loading\n"); */
-    /* net_print_results(net, input, output, &mul_max); */
-    /* net_print_results(net, val_in, val_out, &mul_max); */
+    net_print_results(net, input, output, &mul_max);
+    char *file_name = "lin_reg_model";
+    net_save_to_file(file_name, net);
+    dealloc_net(&net);
+    net = alloc_net_from_file(file_name);
+    printf("After Loading From File\n");
+    net_print_results(net, val_in, val_out, &mul_max);
     return 0;
 }
