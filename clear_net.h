84a37bc94abb6d11d63b60ef626c4993cec8b655
Timothy Cronin <tcroniniv@gmail.com>
2023-07-12 15:46:51 -0400

    format

--- clear_net.h
+++ clear_net.h
@@ -119,7 +119,8 @@ void net_rand(Net net, float low, float high);
 void net_backprop(Net net, Matrix input, Matrix target);
 void net_print_results(Net net, Matrix input, Matrix target,
                        float (*fix_output)(float));
-void net_get_batch(Matrix *batch_input, Matrix* batch_output, Matrix input, Matrix output, size_t batch_num, size_t batch_size);
+void net_get_batch(Matrix *batch_input, Matrix *batch_output, Matrix input,
+                   Matrix output, size_t batch_num, size_t batch_size);
 // void net_forward(Net net);
 
 #endif // CLEAR_NET
@@ -132,7 +133,7 @@ float actf(float x, Activation act) {
     case Sigmoid:
         return 1 / (1 + expf(-x));
     case ReLU:
-	  return x > 0 ? x : 0;
+        return x > 0 ? x : 0;
     case Leaky_ReLU:
         return x >= 0 ? x : CLEAR_NET_ACT_NEG_SCALE * x;
     case Tanh: {
@@ -273,16 +274,16 @@ void mat_mul(Matrix dest, Matrix left, Matrix right) {
 }
 
 void mat_randomize_rows(Matrix mat) {
-  for (size_t i = 0; i < mat.nrows; ++i) {
-    size_t j = i + rand() % (mat.nrows - i);
-    if (i != j) {
-      for (size_t k = 0; k < mat.ncols; ++k) {
-        float t = MAT_GET(mat, i, k);
-        MAT_GET(mat, i, k) = MAT_GET(mat, j, k);
-        MAT_GET(mat, j, k) = t;
-      }
+    for (size_t i = 0; i < mat.nrows; ++i) {
+        size_t j = i + rand() % (mat.nrows - i);
+        if (i != j) {
+            for (size_t k = 0; k < mat.ncols; ++k) {
+                float t = MAT_GET(mat, i, k);
+                MAT_GET(mat, i, k) = MAT_GET(mat, j, k);
+                MAT_GET(mat, j, k) = t;
+            }
+        }
     }
-  }
 }
 
 void mat_write_to_file(FILE *fp, Matrix mat) {
@@ -325,8 +326,8 @@ Net alloc_net(size_t *shape, size_t nlayers) {
         net.weights[i - 1] = alloc_mat(net.activations[i - 1].ncols, shape[i]);
         net.weight_alters[i - 1] =
             alloc_mat(net.activations[i - 1].ncols, shape[i]);
-		// this is to avoid nan errors
-		mat_fill(net.weight_alters[i-1],0);
+        // this is to avoid nan errors
+        mat_fill(net.weight_alters[i - 1], 0);
         // allocate biases as one row and the shape of this layer
         net.biases[i - 1] = alloc_mat(1, shape[i]);
         // allocate activations as one row to add to each
@@ -474,11 +475,11 @@ void net_backprop(Net net, Matrix input, Matrix target) {
             mat_fill(net.buffer[j], 0);
         }
     }
-	
+
     for (size_t i = 0; i < net.nlayers - 1; ++i) {
         for (size_t j = 0; j < net.weights[i].nrows; ++j) {
             for (size_t k = 0; k < net.weights[i].ncols; ++k) {
-			    MAT_GET(net.weights[i], j, k) -=
+                MAT_GET(net.weights[i], j, k) -=
                     MAT_GET(net.weight_alters[i], j, k);
                 // reset for next backpropagation
                 MAT_GET(net.weight_alters[i], j, k) = 0;
@@ -487,9 +488,12 @@ void net_backprop(Net net, Matrix input, Matrix target) {
     }
 }
 
-void net_get_batch(Matrix *batch_input, Matrix* batch_output, Matrix input, Matrix output, size_t batch_num, size_t batch_size) {
-  *batch_input = mat_form(batch_size, input.ncols, input.stride, &MAT_GET(input, batch_num * batch_size, 0));
-  *batch_output = mat_form(batch_size, output.ncols, output.stride, &MAT_GET(output, batch_num * batch_size, 0));
+void net_get_batch(Matrix *batch_input, Matrix *batch_output, Matrix input,
+                   Matrix output, size_t batch_num, size_t batch_size) {
+    *batch_input = mat_form(batch_size, input.ncols, input.stride,
+                            &MAT_GET(input, batch_num * batch_size, 0));
+    *batch_output = mat_form(batch_size, output.ncols, output.stride,
+                             &MAT_GET(output, batch_num * batch_size, 0));
 }
 
 void net_print_results(Net net, Matrix input, Matrix target,
--- examples/mnist.c
+++ examples/mnist.c
@@ -16,10 +16,10 @@ const size_t num_test_files = 10000;
 float fix_output(float x) { return 10 * x; }
 
 int get_data_from_dir(Matrix *data, char *path, int num_files) {
-  DIR *directory = opendir(path);
+    DIR *directory = opendir(path);
     if (directory == NULL) {
         printf("Error: Failed to open %s.\n", path);
-		return 1;
+        return 1;
     }
     struct dirent *entry;
     int count = 0;
@@ -63,68 +63,75 @@ int get_data_from_dir(Matrix *data, char *path, int num_files) {
     closedir(directory);
     CLEAR_NET_ASSERT(count == num_files);
 
-	return 0;
+    return 0;
 }
 
 int main(void) {
     srand(0);
     char *train_path = "./datasets/mnist/train";
-	Matrix train = alloc_mat(num_train_files, num_pixels + 1);
-	int	res = get_data_from_dir(&train, train_path, num_train_files);
-	if (res != 0) {
-		return 1;
+    Matrix train = alloc_mat(num_train_files, num_pixels + 1);
+    int res = get_data_from_dir(&train, train_path, num_train_files);
+    if (res != 0) {
+        return 1;
     }
-	// randomize for stochastic gradient descent
-  	mat_randomize_rows(train);
-	Matrix train_input = mat_form(num_train_files, num_pixels, train.ncols, &MAT_GET(train, 0, 0));
-    Matrix train_output = mat_form(num_train_files, 1, train.ncols, &MAT_GET(train, 0, num_pixels));
+    // randomize for stochastic gradient descent
+    mat_randomize_rows(train);
+    Matrix train_input = mat_form(num_train_files, num_pixels, train.ncols,
+                                  &MAT_GET(train, 0, 0));
+    Matrix train_output = mat_form(num_train_files, 1, train.ncols,
+                                   &MAT_GET(train, 0, num_pixels));
 
-	char *test_path = "./datasets/mnist/test";
-	Matrix test = alloc_mat(num_train_files, num_pixels + 1);
-	res = get_data_from_dir(&test, test_path, num_test_files);
-	if (res != 0) {
-		return 1;
+    char *test_path = "./datasets/mnist/test";
+    Matrix test = alloc_mat(num_train_files, num_pixels + 1);
+    res = get_data_from_dir(&test, test_path, num_test_files);
+    if (res != 0) {
+        return 1;
     }
-	Matrix test_input = mat_form(num_test_files, num_pixels, test.ncols, &MAT_GET(test, 0, 0));
-    Matrix test_output = mat_form(num_test_files, 1, test.ncols, &MAT_GET(test, 0, num_pixels));
+    Matrix test_input =
+        mat_form(num_test_files, num_pixels, test.ncols, &MAT_GET(test, 0, 0));
+    Matrix test_output =
+        mat_form(num_test_files, 1, test.ncols, &MAT_GET(test, 0, num_pixels));
 
-	// size_t shape[] = {num_pixels, 20, 16, 1};
+    // size_t shape[] = {num_pixels, 20, 16, 1};
     size_t shape[] = {num_pixels, 16, 16, 1};
     size_t num_layers = ARR_LEN(shape);
     Net net = alloc_net(shape, num_layers);
     net_rand(net, -1, 1);
     size_t num_epochs = 20000;
     float error;
-    float error_break = 0.02;
+    float error_break = 0.01;
     // Use stochastic gradient descent
     Matrix batch_input;
     Matrix batch_output;
     size_t batch_size = 10;
     CLEAR_NET_ASSERT(num_train_files % batch_size == 0);
     printf("Beginning Training\n");
-	printf("Initial Cost: %f\n", net_errorf(net, train_input, train_output));
+    printf("Initial Cost: %f\n", net_errorf(net, train_input, train_output));
     for (size_t i = 0; i < num_epochs; ++i) {
-      for (size_t batch_num = 0; batch_num < (num_train_files / batch_size);
-           ++batch_num) {
-        net_get_batch(&batch_input, &batch_output, train_input, train_output,
-                      batch_num, batch_size);
-        net_backprop(net, batch_input, batch_output);
-      }
-      error = net_errorf(net, train_input, train_output);
-      printf("Cost after epoch %zu: %f\n", i, error);
-      if (error < error_break) {
-        printf("Less than: %f error after epoch %zu\n", error_break, i);
-        break;
-      }
-	    }
+        for (size_t batch_num = 0; batch_num < (num_train_files / batch_size);
+             ++batch_num) {
+            net_get_batch(&batch_input, &batch_output, train_input,
+                          train_output, batch_num, batch_size);
+            net_backprop(net, batch_input, batch_output);
+        }
+        error = net_errorf(net, train_input, train_output);
+        printf("Cost after epoch %zu: %f\n", i, error);
+        if (error < error_break) {
+            printf("Less than: %f error after epoch %zu\n", error_break, i);
+            break;
+        }
+    }
 
-	printf("Final Error on training set: %f\n", net_errorf(net, train_input, train_output));
-	char* file = "mnist_model";
+    printf("Final Error on training set: %f\n",
+           net_errorf(net, train_input, train_output));
+    char *file = "mnist_model";
     net_save_to_file(file, net);
     dealloc_net(&net);
-	net = alloc_net_from_file(file);
-	printf("After Loading, Error on training set: %f\n", net_errorf(net, train_input, train_output));
-	printf("After Loading, Error on testing set: %f\n", net_errorf(net, test_input, test_output));
+    net = alloc_net_from_file(file);
+    printf("After Loading, Error on training set: %f\n",
+           net_errorf(net, train_input, train_output));
+    printf("After Loading, Error on testing set: %f\n",
+           net_errorf(net, test_input, test_output));
     dealloc_mat(&train);
-	dealloc_mat(&test);
+    dealloc_mat(&test);
 }
