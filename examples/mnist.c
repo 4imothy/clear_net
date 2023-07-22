d6824edc588cf36801a8a260d49c5b5fd8d846b5
Timothy Cronin <tcroniniv@gmail.com>
2023-07-21 22:14:37 -0400

    update output dimension

--- examples/mnist.c
+++ examples/mnist.c
@@ -1,7 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../external/stb_image.h"
 #define CLEAR_NET_IMPLEMENTATION
-#define CLEAR_NET_MOMENTUM 1
+// #define CLEAR_NET_MOMENTUM 1
 #include "../clear_net.h"
 
 #include <dirent.h>
@@ -12,8 +12,9 @@ const size_t img_width = 28;
 const size_t num_pixels = img_height * img_width;
 const size_t num_train_files = 60000;
 const size_t num_test_files = 10000;
+const size_t dim_output = 10;
 
-float fix_output(float x) { return 10 * x; }
+float fix_output(float x) { return x; }
 
 int get_data_from_dir(Matrix *data, char *path, int num_files) {
     DIR *directory = opendir(path);
@@ -56,7 +57,8 @@ int get_data_from_dir(Matrix *data, char *path, int num_files) {
                 MAT_GET(*data, count, j) = img_pixels[j] / 255.f;
             }
             // the python script set it up so the first character is the label
-            MAT_GET(*data, count, num_pixels) = (entry->d_name[0] - '0') / 10.f;
+			size_t label = (entry->d_name[0] - '0');
+	        MAT_GET(*data, count, num_pixels + label) = 1;
             count++;
         }
     }
@@ -68,21 +70,22 @@ int get_data_from_dir(Matrix *data, char *path, int num_files) {
 
 int main(void) {
     srand(0);
+	
     char *train_path = "./datasets/mnist/train";
-    Matrix train = alloc_mat(num_train_files, num_pixels + 1);
+    Matrix train = alloc_mat(num_train_files, num_pixels + dim_output);
     int res = get_data_from_dir(&train, train_path, num_train_files);
-    if (res != 0) {
+    if (res) {
         return 1;
     }
     // randomize for stochastic gradient descent
     mat_randomize_rows(train);
     Matrix train_input = mat_form(num_train_files, num_pixels, train.ncols,
                                   &MAT_GET(train, 0, 0));
-    Matrix train_output = mat_form(num_train_files, 1, train.ncols,
+    Matrix train_output = mat_form(num_train_files, dim_output, train.ncols,
                                    &MAT_GET(train, 0, num_pixels));
 
     char *test_path = "./datasets/mnist/test";
-    Matrix test = alloc_mat(num_train_files, num_pixels + 1);
+    Matrix test = alloc_mat(num_train_files, num_pixels + dim_output);
     res = get_data_from_dir(&test, test_path, num_test_files);
     if (res != 0) {
         return 1;
@@ -90,19 +93,19 @@ int main(void) {
     Matrix test_input =
         mat_form(num_test_files, num_pixels, test.ncols, &MAT_GET(test, 0, 0));
     Matrix test_output =
-        mat_form(num_test_files, 1, test.ncols, &MAT_GET(test, 0, num_pixels));
+        mat_form(num_test_files, dim_output, test.ncols, &MAT_GET(test, 0, num_pixels));
 
-    size_t shape[] = {num_pixels, 16, 16, 1};
+    size_t shape[] = {num_pixels, 16, 16, dim_output};
     size_t num_layers = ARR_LEN(shape);
     Net net = alloc_net(shape, num_layers);
     net_rand(net, -1, 1);
     size_t num_epochs = 20000;
     float error;
-    float error_break = 0.01;
+    float error_break = 0.10;
     // Use stochastic gradient descent
     Matrix batch_input;
     Matrix batch_output;
-    size_t batch_size = 10;
+    size_t batch_size = 100;
     CLEAR_NET_ASSERT(num_train_files % batch_size == 0);
     printf("Beginning Training\n");
     printf("Initial Cost: %f\n", net_errorf(net, train_input, train_output));
@@ -131,13 +134,21 @@ int main(void) {
            net_errorf(net, train_input, train_output));
     printf("After Loading, Error on testing set: %f\n",
            net_errorf(net, test_input, test_output));
-    printf("Actual | Prediction\n");
+    printf("Actual\n");
+	printf("Prediction\n");
+	printf("-----------\n");
     for (size_t i = 0; i < num_test_files; ++i) {
         Matrix in = mat_row(test_input, i);
         mat_copy(NET_INPUT(net), in);
         net_forward(net);
-        printf("%f | ", MAT_GET(test_output, i, 0));
-        printf("%f\n", MAT_GET(NET_OUTPUT(net), 0, 0));
+        for (size_t j = 0; j < dim_output; ++j) {
+          printf("%f |", MAT_GET(test_output, i, j));
+        }
+		printf("\n");
+        for (size_t j = 0; j < dim_output; ++j) {
+          printf("%f |", MAT_GET(NET_OUTPUT(net), 0, j));
+        }
+		printf("\n------------------------------\n");
     }
     dealloc_net(&net, 1);
     dealloc_mat(&train);
