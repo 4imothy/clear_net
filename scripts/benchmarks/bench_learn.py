4eabdfb03e652d05c4505021a6609fa82eb3f469
Timothy Cronin <tcroniniv@gmail.com>
2023-08-01 15:47:17 -0400

    remove pointless comments

--- scripts/benchmarks/bench_learn.py
+++ scripts/benchmarks/bench_learn.py
@@ -6,10 +6,6 @@ import psutil
 import os
 import time
 
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# parent_dir = os.path.dirname(current_dir)
-# bench_dir = 'benchmarks'
-
 
 def time_command(command):
     """Time the given command by running it."""
@@ -25,7 +21,7 @@ def measure_memory_usage(command):
     """Run the test and return mem and time used."""
     start_time = time.time()
     process = subprocess.Popen(command, stdout=subprocess.PIPE,
-                               cwd=parent_dir)
+                               cwd=os.getcwd())
     process.wait()
     end_time = time.time()
 
