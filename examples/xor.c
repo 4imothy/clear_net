f6583359383d88aaa84421fe29d8f001aa20d336
Timothy Cronin <tcroniniv@gmail.com>
2023-11-13 09:18:34 -0500

    fix example

--- examples/xor.c
+++ examples/xor.c
@@ -1,44 +1,52 @@
-#define CLEAR_NET_IMPLEMENTATION
-#include "../clear_net.h"
+#include "../lib/clear_net.h"
+#include <stdio.h>
 
-CLEAR_NET_DEFINE_HYPERPARAMETERS
+#define la cn.la
 
 int main(void) {
     srand(0);
-    cn_default_hparams();
-    cn_set_rate(0.5);
-    Net net = cn_alloc_vani_net(2);
-    cn_alloc_dense_layer(&net, Sigmoid, 2);
-    cn_alloc_dense_layer(&net, Sigmoid, 1);
-    cn_randomize_net(&net, -1, 1);
-    Matrix data = cn_alloc_matrix(4, 3);
-    for (size_t i = 0; i < 2; ++i) {
-        for (size_t j = 0; j < 2; ++j) {
-            size_t row = i * 2 + j;
+    HParams hp = cn.defaultHParams();
+    cn.setRate(&hp, 10);
+    Net *net = cn.allocVanillaNet(hp, 2);
+    cn.allocDenseLayer(net, Sigmoid, 2);
+    cn.allocDenseLayer(net, Sigmoid, 1);
+    cn.randomizeNet(net, -1, 1);
+    cn.printNet(net, "net");
+    Matrix data = la.allocMatrix(4, 3);
+    for (ulong i = 0; i < 2; ++i) {
+        for (ulong j = 0; j < 2; ++j) {
+            ulong row = i * 2 + j;
             MAT_AT(data, row, 0) = i;
             MAT_AT(data, row, 1) = j;
             MAT_AT(data, row, 2) = i ^ j;
         }
     }
+
+    la.printMatrix(&data, "data");
+
     Matrix input =
-        cn_form_matrix(data.nrows, 2, data.stride, &MAT_AT(data, 0, 0));
-    Matrix target = cn_form_matrix(data.nrows, 1, data.stride,
+        la.formMatrix(data.nrows, 2, data.stride, &MAT_AT(data, 0, 0));
+    Matrix target = la.formMatrix(data.nrows, 1, data.stride,
                                    &MAT_AT(data, 0, data.ncols - 1));
-    float loss;
-    size_t num_epochs = 10000;
-    for (size_t i = 0; i < num_epochs; ++i) {
-        loss = cn_learn_vani(&net, input, target);
+    scalar loss;
+    ulong num_epochs = 10000;
+    for (ulong i = 0; i < num_epochs; ++i) {
+        loss = cn.learnVanilla(net, input, target);
         if (i % 100 == 0) {
             printf("Average loss: %f\n", loss);
         }
     }
     printf("Final loss: %g\n", loss);
-    cn_print_vani_results(net, input, target);
-    char *file = "model";
-    cn_save_net_to_file(net, file);
-    cn_dealloc_net(&net);
-    net = cn_alloc_net_from_file(file);
-    cn_print_vani_results(net, input, target);
-    cn_dealloc_net(&net);
-    cn_dealloc_matrix(&data);
+
+    Vector out_store = la.allocVector(target.ncols);
+    for (ulong i = 0; i < data.nrows; ++i) {
+        Vector in = la.formVector(input.ncols, &MAT_AT(input, i, 0));
+        la.printVector(&in, "in");
+        cn.predictDense(net, in, &out_store);
+        la.printVector(&out_store, "out");
+    }
+
+    cn.deallocNet(net);
+    la.deallocMatrix(&data);
+    return 0;
 }
